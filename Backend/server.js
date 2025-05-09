ee494a2a64a24b8d81dbd6f176cfb23af37a7a91
Senithu Thisas <st20306044@outlook.cardiffmet.ac.uk>
2025-05-09 21:45:46 +0530

    add MongoDB connection setup using mongoose

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,13 @@
 //const express = require('express');
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 app.get("/products", (req, res) => {
-    res.send("Server is Ready 123");
+   console.log(process.env.MONGO_URI);
   });
 
 app.listen(5000, () => {
