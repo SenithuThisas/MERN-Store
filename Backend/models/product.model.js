9913597a31d3d8976b36111a1235266dacff3cd4
Senithu Thisas <st20306044@outlook.cardiffmet.ac.uk>
2025-05-10 12:20:38 +0530

    created the product model js file

---
+++ Backend/models/product.model.js
@@ -0,0 +1,33 @@
+import mangoose from "mongoose";
+const productSchema = mangoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    brand: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    countInStock: {
+        type: Number,
+        required: true,
+    },
+}, {
+    timestamps:true
+});
