49fc126c8188a613832966070827600367e78ae5
Senithu Thisas <st20306044@outlook.cardiffmet.ac.uk>
2025-05-17 19:20:10 +0530

    change teh hardcoded port to a variable

--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,18 @@
-//const express = require('express');
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-
-import productRoutes from "./routes/product.route.js";
+import productRoutes from "../routes/product.route.js"; 
 
 dotenv.config();
 
-
-
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json()); //allows us to acccept JASON data in the body of the req.body
+app.use(express.json());
 
-app.use("/api/products", productRoutes)
-
-
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
   connectDB();
-    console.log("Server started at http://localhost:5000");
+  console.log("Server started at http://localhost:"+ PORT);
 });
